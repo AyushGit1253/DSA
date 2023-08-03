9fb5a54471cc28b23a420f12f844a8cc24b806eb
AyushGit1253 <ayush1253khadatkar@gmail.com>
2023-08-03 22:21:45 +0530

    async

--- promiseAll.js
+++ promiseAll.js
@@ -1,4 +1,4 @@
-const posts=[]
+/* const posts=[]
 let lastActivity=''
 function creatPost(post){
     return new Promise((resolve,reject)=>{
@@ -46,4 +46,59 @@ Promise.all([createPostPromise,createPostPromise1,lastActivityPromise]).then(()=
     console.log('last seen:',lastActivity)
 }).catch((error)=>{
     console.log(error)
-})
+}) */
+
+//using async await
+const posts=[]
+let lastActivity=''
+
+function creatPost(post){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            posts.push(post)
+            resolve()
+        },1000)
+    })
+}
+function updateLastUserActivityTime(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            lastActivity= new Date().toLocaleTimeString()
+            resolve()
+        },1000)
+    })
+}
+
+function deleteLastPost(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            if(posts.length>0){
+                var popped=posts.pop()
+                resolve(popped)
+            }
+            else{
+                reject("ERROR")
+            }
+        })
+    })
+}
+async function main(){
+try{
+let createPostPromise=creatPost(['Post1'])
+let createPostPromise1=creatPost(['Post2'])
+
+let lastActivityPromise=updateLastUserActivityTime()
+
+let[post,post1,last]=await Promise.all([createPostPromise,createPostPromise1,lastActivityPromise])
+    console.log(posts)
+    console.log('last seen:',lastActivity)
+
+let deletePost= await deleteLastPost()
+    console.log(deletePost)
+    console.log(posts)
+    console.log('last seen:',lastActivity)
+}catch(error){
+    console.log(error)
+}
+}
+main()
